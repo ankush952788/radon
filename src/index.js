e06256408bba3f2c588353611d2daa0531ee5731
ankush <ankushharne@02gamil.com>
2022-06-08 10:51:42 +0530

    done

--- src/index.js
+++ src/index.js
@@ -10,3 +10,33 @@ app.use('/', route);
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const route = require('./routes/route.js');
+// const mongoose = require('mongoose');
+// const app = express();
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// mongoose.connect("mongodb+srv://ankushharne:anku@123@cluster0.4c2z9xn.mongodb.net/?retryWrites=true&w=majority",{
+//     useNewUrlParser: true
+// })
+// .then( () => console.log("MongoDb is connected"))
+// .catch ( err => console.log(err) )
+// app.use('/', route);+
+
+
+// app.listen(process.env.PORT || 3000, function () {
+//     console.log('Express app running on port ' + (process.env.PORT || 3000))
+// });
