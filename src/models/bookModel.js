940aa373d44407e733a2439f4c90ff560f05d0bf
ankush <ankushharne@02gamil.com>
2022-06-13 11:02:04 +0530

    done

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,22 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose=require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
+const bookSchema = new mongoose.Schema({
+
+    bookName:{
+        type:String,
+        required: true
+},
+authorName:String,
+category :String,
+totalPages :Number,
+stockAvailable : Boolean,
+prices: {
+    indianPrice: String,
+    europePrice: String,
+},
+year:{
+    type:Number,
+}},{timestamps:true}
+)
+
+module.exports = mongoose.model('Book', bookSchema) 
+
+
+
+
+
+// const mongoose = require('mongoose');
+
+// const bookSchema = new mongoose.Schema( {
+//     bookName: String, 
+//     authorName: String, 
+//     tags: [String],
     
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
-    },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
+//     isPublished: Boolean,
+//     prices: {
+//         indianPrice: String,
+//         europePrice: String,
+//     },
+//     sales: {type: Number, default: 10}
+// }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+// module.exports = mongoose.model('Book', bookSchema) //users
 
-//Validation:
+// //Validation:
 //require:true
 //unique
 // default
